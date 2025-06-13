294d12010aaa0f3b82df15e292d8d5bc40a274cc
nexcraftaipro <nexcraftaipro@gmail.com>
2025-06-13 10:59:13 +0600

    Update Pricing.tsx

--- src/pages/Pricing.tsx
+++ src/pages/Pricing.tsx
@@ -56,7 +56,7 @@ const PricingPage: React.FC = () => {
               <CardHeader className="pb-0">
                 <h2 className="text-2xl font-bold text-white">Premium</h2>
                 <div className="flex items-baseline mt-2">
-                  <span className="text-5xl font-extrabold tracking-tight">1000</span>
+                  <span className="text-5xl font-extrabold tracking-tight">2000</span>
                   <span className="ml-1 text-slate-50 text-xl">Tk/Yearly</span>
                 </div>
                 <div className="mt-1">
